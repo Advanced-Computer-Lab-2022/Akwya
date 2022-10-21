33ceae92f9ab5b5f2ccf0a17c119753c4bfc0a1a
yasmineashraff <yasmineaabyad@gmail.com>
2022-10-21 20:34:07 +0200

    commit4

--- client/src/App.js
+++ client/src/App.js
@@ -1,44 +1,44 @@
-import React, {useEffect} from 'react';
-import{Container,AppBar,Typography,Grow,Grid} from '@mui/material';
-import {useDispatch} from 'react-redux';
+import React, { useEffect } from 'react';
+import { Container, AppBar, Typography, Grow, Grid } from '@mui/material';
+import { useDispatch } from 'react-redux';
 
-import {getPosts} from './actions/posts';
+import { getPosts } from './actions/posts';
 
 import Posts from './components/Posts/Posts';
 import Form from './components/Form/Form';
 import './App.css';
-import '/Users/farahwanas/Desktop/Akwya/client/src/components/Posts/Post/Post.css';
-import '/Users/farahwanas/Desktop/Akwya/client/src/components/Posts/Posts.css';
-import '/Users/farahwanas/Desktop/Akwya/client/src/components/Form/Form.css';
-
+import './components/Posts/Post/Post.css';
+import './components/Posts/Posts.css';
+import './components/Form/Form.css';
+//yaso
 
 import photo from './images/photo.jpeg';
 
 const App = () => {
-    const dispatch=useDispatch();
-    useEffect(()=>{
+    const dispatch = useDispatch();
+    useEffect(() => {
         dispatch(getPosts());
     }, [dispatch]);
-    
 
 
-    return(
+
+    return (
 
 
         <Container maxwidth="lg">
             <AppBar className="appBar" position="static" color="inherit">
-            <Typography className="headings" variant="h2" align="left" >Online Learning System</Typography>
-            <img  classname="img" src={photo} alt="A Photo" height="60"/>
+                <Typography className="headings" variant="h2" align="left" >Online Learning System</Typography>
+                <img classname="img" src={photo} alt="A Photo" height="60" />
             </AppBar>
-          
+
             <Grow in>
                 <Container>
                     <Grid container justify="space-between" alignItems="stretch" spacing={3}>
                         <Grid item xs={12} sm={7}>
-                             <Posts/>
+                            <Posts />
                         </Grid>
                         <Grid item xs={12} sm={4}>
-                            <Form/>
+                            <Form />
                         </Grid>
                     </Grid>
                 </Container>
