65f7942fd029f0576bdd64da8a8406a9f474f830
yasmineashraff <yasmineaabyad@gmail.com>
2022-10-25 19:15:55 +0200

    Imports

--- client/src/App.js
+++ client/src/App.js
@@ -6,49 +6,32 @@ import Navbar from './components/Navbar'
 import Admin from './pages/Admin';
 import User from './pages/User';
 
-import Posts from './components/Posts/Posts';
-import Form from './components/Form/Form';
-import './App.css';
-import './components/Posts/Post/Post.css';
-import './components/Posts/Posts.css';
-import './components/Form/Form.css';
-
-import photo from './images/photo.jpeg';
-
-const App = () => {
-  const dispatch = useDispatch();
-  useEffect(() => {
-    dispatch(getPosts());
-  }, [dispatch]);
-
-
+function App() {
 
   return (
-
-
-    <Container maxwidth="lg">
-      <AppBar className="appBar" position="static" color="inherit">
-        <Typography className="headings" variant="h2" align="left" >Online Learning System</Typography>
-        <img classname="img" src={photo} alt="A Photo" height="60" />
-      </AppBar>
-
-      <Grow in>
-        <Container>
-          <Grid container justify="space-between" alignItems="stretch" spacing={3}>
-            <Grid item xs={12} sm={7}>
-              <Posts />
-            </Grid>
-            <Grid item xs={12} sm={4}>
-              <Form />
-            </Grid>
-          </Grid>
-        </Container>
-      </Grow>
-    </Container>
-  )
-
+    <div className="App">
+      <BrowserRouter>
+        <Navbar />
+        <div className="pages">
+          <Routes>
+            <Route
+              path="/"
+              element={<Home />}
+            />
+            <Route
+              path="/admin"
+              element={<Admin />}
+            />
+            <Route
+              path="/user"
+              element={<User />}
+            />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </div>
+  );
 
 }
 
 export default App;
-
