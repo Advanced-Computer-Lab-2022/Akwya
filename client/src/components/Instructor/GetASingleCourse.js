91151d96682a20a9a12a2547cca78bb9e5612699
Youssef Amer <Youssef Amer@YOUSSEFAMER>
2022-11-01 02:17:43 +0200

    Add countries and currencies to several pages

--- client/src/components/Instructor/GetASingleCourse.js
+++ client/src/components/Instructor/GetASingleCourse.js
@@ -3,7 +3,7 @@
 import react, {useState, useEffect} from 'react'
 import axios from 'axios'
 
-function MyCourse() {
+function MyCourse(props) {
 const [courses,setCourses] = useState([])
 
 const ID = window.location.href.split('/').at(5);
@@ -18,13 +18,28 @@ useEffect(()=>{
     .catch(err=>{console.log(err)})
 },[])
 
+let rate = 1;
+let currency = 'GBP'
+switch(props.country) {
+    case 'United States':
+      rate = 1.15;
+      currency = 'USD';
+      break;
+    case 'Egypt (‫مصر‬‎)':
+    // case 'Egypt':
+      rate=27.85;
+      currency = 'EGP';
+      break;
+    default:
+      // rate = 3;
+  }
 
 return(
     <div>
         <h1>Course Details</h1>
         <ul>
 
-            {courses.map(course => <li key={course._id}>Title: {course.title} Price: {course.price} Total Hours: {course.totalHours} Rating: {course.rating} Summary: {course.summary}</li>)}
+            {courses.map(course => <li key={course._id}>Title: {course.title} Price: {(Math.round(course.price * rate) + ' ' + currency)} Total Hours: {course.totalHours} Rating: {course.rating} Summary: {course.summary}</li>)}
            
 
         </ul>
