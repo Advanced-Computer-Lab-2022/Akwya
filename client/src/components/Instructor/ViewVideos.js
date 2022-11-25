b247a349d4b43926a1537543257a72bc4457ea85
Youssef Amer <Youssef Amer@YOUSSEFAMER>
2022-11-25 23:12:48 +0200

    Fix to URL in videos

--- client/src/components/Instructor/ViewVideos.js
+++ client/src/components/Instructor/ViewVideos.js
@@ -40,7 +40,7 @@ return(
         <ul>
 
             {/* {videos.map(video => <li key={video._id}>Title: {video.title} Price: {(Math.round(video.price * rate) + ' ' + currency)} Total Hours: {video.totalHours} Rating: {video.rating} Summary: {video.summary}</li>)} */}
-            {videos.map(video => <div><h1>{video.title}:</h1><h3>Total Hours: {video.totalHours}</h3><iframe width="700" height="350" src={video.url} title={video.title} frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+            {videos.map(video => <div><h1>{video.title}:</h1><h3>Total Hours: {video.totalHours}</h3><iframe width="700" height="350" src={'https://www.youtube.com/embed/'+video.url.split('=').at(1)} title={video.title} frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
             <br/><small>summary: {video.summary}</small></div>)}
            
 
