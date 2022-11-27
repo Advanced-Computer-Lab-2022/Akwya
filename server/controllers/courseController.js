5a88fe419eccd8bb9920a78128e327cd37a72b07
omarkamal280 <omarkamal619@gmail.com>
2022-11-27 23:46:57 +0200

    kheir

--- server/controllers/courseController.js
+++ server/controllers/courseController.js
@@ -122,6 +122,7 @@ const createCourse = async (req, res) => {
      const instructor = req.params.id
 
     const previewVideo = ""
+    const noOfRatings=0
     try {
         const newCourse = await course.create({
             title,
