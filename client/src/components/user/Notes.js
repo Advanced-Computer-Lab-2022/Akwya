83b5a386128e67200d5d5d1dc3c00bbb5e740d21
Youssef Amer <Youssef Amer@YOUSSEFAMER>
2022-12-19 17:47:41 +0200

    Notes Done With Download

---
+++ client/src/components/user/Notes.js
@@ -0,0 +1,89 @@
+import React, { useState } from "react";
+import Swal from "sweetalert2";
+
+
+
+const Notes = () => { 
+
+    const [sidebarOpen, setSideBarOpen] = useState(false);
+
+    var textarea = ''
+
+  const handleViewSidebar = () => {
+    setSideBarOpen(!sidebarOpen);
+  };
+
+    const SideBar = props => {
+        const sidebarClass = props.isOpen ? "0" : "-20vw";
+        return (
+            
+          <div style={{"position": "fixed", "top": "160px", "right":sidebarClass, "height": "100%","width": "20vw", "transition": "right 0.3s ease-in-out", "background-color": "lightgray"}}>
+            <div style={{ "text-align" : 'center' }}>
+            <form onSubmit={(e)=>{
+              e.preventDefault();downloadTxtFile()}}>
+              <textarea rows="26" cols="32" onChange={(e) => {textarea = e.target.value}} style={{"font-size":"12pt"}}></textarea>
+              <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
+              <button class="btn" id="downloadbtn"  style={{"marginTop":"20px","background-color": "#1976d2",
+               "border": "none", "color": "white", "padding": "12px", "cursor": "pointer", "font-size": "20px",  "border-radius":"5px"}}>
+                <i class="fa fa-download"></i> Download</button>
+                </form>
+            </div>
+            <button onClick={props.toggleSidebar} className="sidebar-toggle" style={{"position": "absolute",  "top": "20%", 
+            "left":" -80px", "height":"60px", "width": "80px", "z-index": "1", "background-color": "#1976d2",
+            "border": "none", "color": "white", "padding": "12px", "cursor": "pointer", "font-size": "20px", "border-radius":"20px 0px 0px 20px"}}>
+            Notes
+            </button>
+          </div>
+        );
+      };
+      
+      function Bar() {
+        const [sidebarOpen, setSideBarOpen] = useState(false);
+        const handleViewSidebar = () => {
+          setSideBarOpen(!sidebarOpen);
+        };
+        return (
+          <span>
+            <SideBar isOpen={sidebarOpen} toggleSidebar={handleViewSidebar} />
+          </span>
+        );
+      }
+    
+      function downloadTxtFile() {
+        if(textarea==''){
+            Swal.fire({
+                title: 'Your Notes Are Empty',
+                icon: 'info',
+                confirmButtonColor: '#1976d2',
+                confirmButtonText: 'OK',
+                confirmButtonColor: '#1976d2'
+    
+                
+              })
+            return
+        }
+        const element = document.createElement("a");
+        const file = new Blob([textarea],    
+                    {type: 'text/plain;charset=utf-8'});
+        element.href = URL.createObjectURL(file);
+        element.download = "myNotes.txt";
+        document.body.appendChild(element);
+        element.click();
+      }
+
+
+    return(
+        <div>
+        <Bar isOpen={sidebarOpen} toggleSidebar={handleViewSidebar}/>
+       </div>
+    )
+
+
+
+}
+export default Notes
+
+
+
+
+  
--- client/src/pages/Course.js
+++ client/src/pages/Course.js
@@ -12,42 +12,12 @@ import CssBaseline from '@mui/material/CssBaseline';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
+import Notes from '../components/user/Notes.js'
+
 
 
 import reportAProblem2 from '../components/Problem/reportAProblem2'
 
-const SideBar = props => {
-  const sidebarClass = props.isOpen ? "0" : "-20vw";
-  return (
-    <div style={{"position": "fixed", "top": "160px", "right":sidebarClass, "height": "100%","width": "20vw", "transition": "right 0.3s ease-in-out", "background-color": "lightgray"}}>
-      <div style={{ "text-align" : 'center' }}>
-        <textarea rows="26" cols="32" style={{"font-size":"12pt"}}></textarea>
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
-        <button class="btn" style={{"marginTop":"20px","background-color": "#1976d2",
-         "border": "none", "color": "white", "padding": "12px", "cursor": "pointer", "font-size": "20px",  "border-radius":"5px"}}>
-          <i class="fa fa-download"></i> Download</button>
-
-      </div>
-      <button onClick={props.toggleSidebar} className="sidebar-toggle" style={{"position": "absolute",  "top": "20%", 
-      "left":" -80px", "height":"60px", "width": "80px", "z-index": "1", "background-color": "#1976d2",
-      "border": "none", "color": "white", "padding": "12px", "cursor": "pointer", "font-size": "20px", "border-radius":"20px 0px 0px 20px"}}>
-      Notes
-      </button>
-    </div>
-  );
-};
-
-function Bar() {
-  const [sidebarOpen, setSideBarOpen] = useState(false);
-  const handleViewSidebar = () => {
-    setSideBarOpen(!sidebarOpen);
-  };
-  return (
-    <span>
-      <SideBar isOpen={sidebarOpen} toggleSidebar={handleViewSidebar} />
-    </span>
-  );
-}
  
 
 const Course = (props) => {
@@ -119,32 +89,15 @@ const Course = (props) => {
 
 
 
-  function downloadTxtFile() {
-    const element = document.createElement("a");
-    const file = new Blob([document.getElementById('input').value],    
-                {type: 'text/plain;charset=utf-8'});
-    element.href = URL.createObjectURL(file);
-    element.download = "myFile.txt";
-    document.body.appendChild(element);
-    element.click();
-   
-  }
-
     return (
       <div className="guest">
         <h2>Course Page</h2>
         <Link to="/">
           <h2>Go to Home Page</h2>
         </Link>
-        <div>
-         <input id="input" />
-         <button onClick={downloadTxtFile}>Download</button>  
-        </div>
-<Bar isOpen={sidebarOpen} toggleSidebar={handleViewSidebar}/>
-
         <TabGroup/>
 
-        <div style={{display: showContent ? 'block' : 'none' , "text-align" : 'center' }}><UserViewVideos/></div>
+        <div style={{display: showContent ? 'block' : 'none' , "text-align" : 'center' }}><UserViewVideos/><Notes/></div>
         <div style={{display: showDetails ? 'block' : 'none' }}><MyCourse country={props.country}/>
 
         <RateAndReviewCourse/>
