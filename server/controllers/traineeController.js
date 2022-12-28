f3a99a3f4300e7dc644029333cf265d2249c85a9
Youssef Amer <Youssef Amer@YOUSSEFAMER>
2022-12-28 03:03:36 +0200

    Show My Courses For Trainee

--- server/controllers/traineeController.js
+++ server/controllers/traineeController.js
@@ -433,6 +433,24 @@ const sendCertificate = async (req,res)=>{
         }
     }
 
+    const myCourses= async(req,res) => {
+        try{
+
+            const courses= await courseBought.find({TraineeID:req.params.TraineeID})
+
+
+
+
+        
+            res.status(200).json(courses)
+        }
+    
+        catch(error){
+            res.status(400).json({message: error.message})
+        }
+    }
+
 export {getTrainee,registerCourse,isRegistered,dropCourse,rateCourse,changePassword,rateInstructor,checkPassword,
-    resetPassword,getWallet,videoCount,sendCertificate,signUp,login,logout,userWatchVideo,getUserProgress,refundCourse,requestRefund}
+    resetPassword,getWallet,videoCount,sendCertificate,signUp,login,logout,userWatchVideo,getUserProgress,
+    refundCourse,requestRefund,myCourses}
 
